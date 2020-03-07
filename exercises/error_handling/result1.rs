c86d54282254ab0b74b6cdddead056e8721e9116
Manan Khattar <khattar.manan@gmail.com>
2020-03-07 22:24:36 +0000

    did as much as i thought was pertinent

--- exercises/error_handling/result1.rs
+++ exercises/error_handling/result1.rs
@@ -1,8 +1,6 @@
 // result1.rs
 // Make this test pass! Execute `rustlings hint result1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -14,7 +12,11 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        Ok(PositiveNonzeroInteger(value as u64))
+        match (value > 0, value == 0) {
+            (true, _) => Ok(PositiveNonzeroInteger(value as u64)),
+            (false, true) => Err(CreationError::Zero),
+            (false, false) => Err(CreationError::Negative),
+        }
     }
 }
 
